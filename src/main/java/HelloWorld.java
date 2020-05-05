a52d114e57a9cc84c1e0ced6662c5cd611b2fb6e
Oliver <oliver.goldbart@gmail.com>
2020-05-05 11:14:20 -0400

    Cleaning up some detritus

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,22 +1,5 @@
-import model.User;
-
-import java.io.PrintStream;
-
 public class HelloWorld {
     public static void main(String[] args) {
-        print(System.out);
-        createAlice();
-    }
-
-    public static void print(PrintStream out) {
-        out.print("Hello, World!");
-
-        User alice = new User("Alice");
-    }
-
-    public static String createAlice() {
-        User alice = new User("Alice");
-        System.out.print("Hello, World!");
-        return "Hello, World!";
+        System.out.println("Hello, World!");
     }
 }
--- src/main/java/utils/DateUtil.java
+++ src/main/java/utils/DateUtil.java
@@ -4,7 +4,6 @@ import java.util.Calendar;
 
 public class DateUtil {
     public static String getCurrentDateTime() {
-        // TODO: 5/5/2020 yeah fix this
         return Calendar.getInstance().getTime().toString();
     }
 }
